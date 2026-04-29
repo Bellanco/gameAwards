b7b6f86a6f3b2126096c41c184a195123a162497
tu_nombre <tu_email@github.com>
2026-04-29 18:19:08 +0200

    feat: update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,16 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // IMPORTANTE: Reemplaza estos valores con tus credenciales de la Consola de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyD46f7xoaS9Nf8pZ3_tOxo96IJTiaWo5y4",
+  authDomain: "game-awards-d7881.firebaseapp.com",
+  projectId: "game-awards-d7881",
+  storageBucket: "game-awards-d7881.firebasestorage.app",
+  messagingSenderId: "321669895248",
+  appId: "1:321669895248:web:2bdce94f9d0677b22cad55",
+  measurementId: "G-5ZZGWP5CGQ"
+
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
